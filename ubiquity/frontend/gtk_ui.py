34a47c071f8781eb1c56e55df47a219777e5b7d3
Benjamin Ryzman <benjamin@jolicloud.org>
2010-09-06 12:45:10 +0200

    Reapply "always display installing instead of checking then installing"

--- ubiquity/frontend/gtk_ui.py
+++ ubiquity/frontend/gtk_ui.py
@@ -468,7 +468,7 @@ class Wizard(BaseFrontend):
 
         if 'UBIQUITY_AUTOMATIC' in os.environ:
             self.debconf_progress_start(0, self.pageslen,
-                self.get_string('ubiquity/install/checking'))
+                self.get_string('ubiquity/install/title'))
             self.debconf_progress_window.set_title(
                 self.get_string('ubiquity/install/title'))
             self.install_progress_window.set_title(
